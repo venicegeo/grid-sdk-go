a3aec5222ac5c8df40ac5c37256ef482f01e85fe
Bradley J Chambers <brad.chambers@gmail.com>
2016-02-17 22:57:25 -0500

    Try with just one file, not OS X

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ go test */**_test.go
 
 if [[ $TRAVIS_TAG =~ ^v[0-9\.]+ ]]; then
   go get github.com/mitchellh/gox
-  gox -os="darwin linux windows" -arch="amd64" -build-toolchain
-  gox -os="darwin linux windows" -arch="amd64" -output="grid_{{.OS}}_x86_64"
+  gox -os="linux" -arch="amd64" -build-toolchain
+  gox -os="linux" -arch="amd64" -output="grid_{{.OS}}_x86_64"
 fi

e4fcd382d1e6ac5dc5fa9418caf36d67db6b03c2
Bradley J Chambers <brad.chambers@gmail.com>
2016-02-17 22:45:26 -0500

    Still working on deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,8 @@
 
 go test */**_test.go
 
-go get github.com/mitchellh/gox
-gox -os="darwin linux windows" -arch="amd64" -build-toolchain
-gox -os="darwin linux windows" -arch="amd64" -output="{{.Dir}}_{{.OS}}_x86_64"
+if [[ $TRAVIS_TAG =~ ^v[0-9\.]+ ]]; then
+  go get github.com/mitchellh/gox
+  gox -os="darwin linux windows" -arch="amd64" -build-toolchain
+  gox -os="darwin linux windows" -arch="amd64" -output="grid_{{.OS}}_x86_64"
+fi

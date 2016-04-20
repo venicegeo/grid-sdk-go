1fecf5faab65604858f026e3d70c02d73ed3391f
Bradley J Chambers <brad.chambers@gmail.com>
2016-04-20 18:03:20 -0400

    Retry Travis deploy to GitHub Releases

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-go test */**_test.go
-
-if [[ $TRAVIS_TAG =~ ^v[0-9\.]+ ]]; then
-  go get github.com/mitchellh/gox
-  gox -os="linux" -arch="amd64" -build-toolchain
-  gox -os="linux" -arch="amd64" -output="grid_{{.OS}}_x86_64"
-fi
+gox -osarch="darwin/amd64 linux/amd64 windows/amd64" \
+    -output "dist/{{.Dir}}-{{.OS}}-{{.Arch}}"
